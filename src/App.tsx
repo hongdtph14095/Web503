c903d276c01ebd67c34b0d6122d6cd53743fa32a
hongdtph14095 <77144191+hongdtph14095@users.noreply.github.com>
2022-03-29 16:43:57 +0700

    upadte

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import axios from 'axios';
 import './App.css'
- import {add, list, remove} from './api/product';
-import ShowInfo from './cotrollers/ShowInfo';
+ import {add, list, remove, update} from './api/product';
+// import ShowInfo from './cotrollers/ShowInfo';
 import type { ProductType } from './types/product'
 import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import Home from './pages/Product'
@@ -13,8 +13,12 @@ import ManagerProduct from './pages/ManagerProduct';
 import WebsiteLayout from './pages/layouts/WebsiteLayout';
 import AdminLayout from './pages/layouts/AdminLayout';
 import "bootstrap/dist/css/bootstrap.min.css"
+import ShowInfo from './components/ShowInfo';
 
 import ProductAdd from './pages/productAdd';
+import ProductEdit from './pages/ProductEdit';
+import Test from './components/test';
+import PrivateRouter from './components/PrivateRouter';
 
 function App() {
   const [products, setProducts] = useState<ProductType[]>([]);
@@ -22,19 +26,25 @@ function App() {
   //const [count, setCount] = useState<number>(0);
 
   useEffect(() => {
-    const getProducts = async () => {
+    const getProducts = async () => { 
       const { data } = await list();
       setProducts(data);
     }
     getProducts();
   }, [])
-  const removeItem = async(id:number) =>{
-    const {data} = await remove(id);
-    data && setProducts(products.filter(item => item._id !== data._id));
+  const onHandleRemove = async(id:number) =>{
+    // const {data} = await remove(id);
+    // data && setProducts(products.filter(item => item._id !== data._id));
+    await remove(id);
+
   }
   const onHandleAdd = async (product: ProductType) =>{
    const {data} = await add(product);
    setProducts([...products, data])
+  }
+  const onHandleUpdate  = async (product: ProductType) =>{
+    const {data} = await update(product);
+    setProducts(products.map(item => item.id === data.id ? data:item ));
   }
   return (
     
@@ -42,16 +52,18 @@ function App() {
           {/* <Route path='/' element={<h1>Home Page</h1>} />
           < Route path='product' element={<h1>Product Page</h1>} /> */}
           <Route path="/" element={<WebsiteLayout />}>
-            <Route index element={<Navigate to="dashboard" />} />
+            <Route index element={<Home/>} />
             <Route path='product' element={<Product />} />
+            <Route path='login' element={<h1>Login page</h1>} />
             </Route >
-          <Route path='admin' element={< AdminLayout />} >
+          <Route path='admin' element={ <PrivateRouter> < AdminLayout /></PrivateRouter>} >
               <Route index element={< Navigate to="dashboard" />} />
               <Route path='dashboard' element={<Dashboard />} />
-              <Route path='product' element={<ManagerProduct data={products} />} />
-              <Route path='/admin/product/add' element={<ProductAdd onAdd={onHandleAdd}/>}  />
-
-              {/* </Route> */}
+              <Route path="product" >
+              <Route index element={<ManagerProduct data={products} onRemove={onHandleRemove} />} />
+              <Route path='add' element={<ProductAdd onAdd={onHandleAdd}/>}  />
+              <Route path=':id/edit' element={<ProductEdit onUpdate={onHandleUpdate}/>}  />
+              </Route>
               </Route >
       </Routes>
 
